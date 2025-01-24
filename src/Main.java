ac4484cd75cf5a728224b28ef9f0caaac1580195
jsimmons_simlow <jsimmons@simlowsoftware.com>
2025-01-24 07:46:43 -0500

    Added the utilities class and included a call to the celsiusToFahrenheit function in the Main class.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // Call the celsiusToFahrenheit function from the Utilities class
+        System.out.println(Utilities.celsiusToFahrenheit(5));
     }
 }
---
+++ src/Utilities.java
@@ -0,0 +1,7 @@
+public class Utilities {
+
+    // Create a function that converts celsius to fahrenheit
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+}
